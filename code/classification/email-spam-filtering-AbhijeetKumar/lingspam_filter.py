faca90132ce09f544f3571acef05022985d8d93a
mamintoosi <m.amintoosi@gmail.com>
2019-05-25 12:14:01 +0430

    debug lingspam_filter

--- code/classification/email-spam-filtering-AbhijeetKumar/lingspam_filter.py
+++ code/classification/email-spam-filtering-AbhijeetKumar/lingspam_filter.py
@@ -8,6 +8,7 @@ Modified by: M.Amintoosi
 
 import os
 import numpy as np
+from numpy  import array
 from collections import Counter
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
@@ -60,9 +61,10 @@ dictionary = make_Dictionary(train_dir)
 
 # Prepare feature vectors per training mail and its labels
 
-train_labels = np.zeros(702)
-train_labels[351:701] = 1
+# train_labels = np.zeros(702)
+# train_labels[351:701] = 1
 train_matrix = extract_features(train_dir)
+train_labels = [fi.startswith('spm') for fi in os.listdir(train_dir)]
 
 # Training SVM and Naive bayes classifier and its variants
 
@@ -77,8 +79,9 @@ model2.fit(train_matrix,train_labels)
 
 test_dir = 'lingspam_public/test-mails'
 test_matrix = extract_features(test_dir)
-test_labels = np.zeros(260)
-test_labels[130:260] = 1
+test_labels = [fi.startswith('spm') for fi in os.listdir(test_dir)]
+# test_labels = np.zeros(260)
+# test_labels[130:260] = 1
 
 result1 = model1.predict(test_matrix)
 result2 = model2.predict(test_matrix)
