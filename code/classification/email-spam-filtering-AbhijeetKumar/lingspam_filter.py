c8b6d9a393a275eb16f1a8348076405210bdbd8d
mamintoosi <m.amintoosi@gmail.com>
2019-05-25 10:32:49 +0430

    debug lingspam_filter

--- code/classification/email-spam-filtering-AbhijeetKumar/lingspam_filter.py
+++ code/classification/email-spam-filtering-AbhijeetKumar/lingspam_filter.py
@@ -67,7 +67,7 @@ train_matrix = extract_features(train_dir)
 # Training SVM and Naive bayes classifier and its variants
 
 model1 = LinearSVC(max_iter=1000000)
-print(svc.get_params()['max_iter'])
+print(model1.get_params()['max_iter'])
 model2 = MultinomialNB()
 
 model1.fit(train_matrix,train_labels)
